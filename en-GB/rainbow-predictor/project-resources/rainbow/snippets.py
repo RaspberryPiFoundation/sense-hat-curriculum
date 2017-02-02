9db0672b14d72e621eff1ac81f8ff782af30227e
Tracy Gardner <tracygardner@users.noreply.github.com>
2017-02-02 15:09:48 +0000

    Added snippets.py file

---
+++ en-GB/rainbow-predictor/project-resources/rainbow/snippets.py
@@ -0,0 +1,10 @@
+rainbow = [
+R, R, R, R, R, R, R, R,
+R, O, O, O, O, O, O, O,
+R, O, Y, Y, Y, Y, Y, Y,
+R, O, Y, G, G, G, G, G,
+R, O, Y, G, B, B, B, B,
+R, O, Y, G, B, I, I, I,
+R, O, Y, G, B, I, V, V,                                                                   
+R, O, Y, G, B, I, V, X
+]
